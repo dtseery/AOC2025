208a4b5e6efe7674342fdaa9bdbeeb92a03a1846
dtseery <owner@DESKTOP-FR500DN.localdomain>
2026-06-27 18:09:52 -0500

    merged updates and start of 9

---
+++ AOC/0/0.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+unsigned long long sum = 0;
+
+int main(int argc, char** argv) {
+	FILE* fp = NULL;
+	//buffer & len for input
+	fp = fopen(*(argv+1), "r");
+	if (fp == NULL) {
+		printf("Error opening file. Make sure file exists.\\n");
+		return -1; // Exit the program with an error
+	}
+	char* buf = NULL;
+	size_t uLen = 0;
+	while(getline(&buf, &uLen, fp) != -1) {
+		//length of line
+		int lenB = strlen(buf);
+}
