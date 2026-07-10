a6d08b7b977682d92620986e26320357e2b79b42
dtseery <theanonymousme79@gmail.com>
2026-07-10 11:10:23 -0500

    a couple of different solution to test for rotating calipers for AOC 9

--- AOC/9/9possiblybest.c
+++ AOC/9/9possiblybest.c
@@ -15,18 +15,18 @@ typedef struct {
 
 int pointCompare (const void* pA, const void* pB) 
 {
-    int x1 = ((Point*)pA)->x;
-    int x2 = ((Point*)pB)->x;
-    int y1 = ((Point*)pA)->y;
-    int y2 = ((Point*)pB)->y;
-    if (x1 > x2) return 1;
-    if (x2 < x1) return -1;
-    if (y1 > y2) return 1;
-    if (y2 < y1) return -1;
-    return 0;
+	int x1 = ((Point*)pA)->x;
+	int x2 = ((Point*)pB)->x;
+	int y1 = ((Point*)pA)->y;
+	int y2 = ((Point*)pB)->y;
+	if (x1 > x2) return 1;
+	if (x2 < x1) return -1;
+	if (y1 > y2) return 1;
+	if (y2 < y1) return -1;
+	return 0;
 }
 unsigned long long dist(Point a, Point b) {
-	return (abs((long long)(a.x-b.x))+1) * (abs((long long)(a.y-b.y))+1);
+	return abs((long long)(a.x-b.x)) * abs((long long)(a.y-b.y));
 };
 
 unsigned long long absArea (Point a, Point b, Point c) {
@@ -44,17 +44,17 @@ Point* convHull(Point* points, int size) {
 	int hullsize = 0;
 	int hullcap = 4;
 	Point* hull = calloc(hullcap, sizeof(Point));
-        qsort(points, size, sizeof(*points), pointCompare);
+	qsort(points, size, sizeof(*points), pointCompare);
 	//one by one
 	for(int i = 0; i<size; i++) {
 		//printf("point %d: %llu, %llu \n", i, points[i].x, points[i].y);	
-		
-		if(hullsize >= 2) printf("area here: %lld\n", crossProduct(hull[hullsize-2], hull[hullsize-1], points[i]));	
+
+		//if(hullsize >= 2) printf("area here: %lld\n", crossProduct(hull[hullsize-2], hull[hullsize-1], points[i]));	
 		while(hullsize >= 2 && crossProduct(hull[hullsize-2], hull[hullsize-1], points[i]) <= 0) {
 			memset(hull+hullsize-1, 0, sizeof(Point));
 			hullsize--;	
 		}
-		
+
 		//before adding, check if we need to increase our array size
 		if(hullsize == hullcap) {
 			hullcap<<=1;
@@ -64,15 +64,15 @@ Point* convHull(Point* points, int size) {
 		//add point at i
 		hull[hullsize] = points[i];
 		hullsize++;
-		
+
 	}
-	
+
 	for(int i = size-2, t = hullsize+1; i>=0; i--) {
 		while(hullsize >= t && crossProduct(hull[hullsize-2], hull[hullsize-1], points[i]) <= 0 ) {
 			memset(hull+hullsize-1, 0, sizeof(Point));
 			hullsize--;
 		}
-		
+
 		//before adding, check if we need to increase our array size
 		if(hullsize == hullcap) {
 			hullcap<<=1;
@@ -84,8 +84,8 @@ Point* convHull(Point* points, int size) {
 		hullsize++;
 
 	}
-	
-	
+
+
 	memset(hull+hullsize-1, 0, sizeof(Point));
 	hullsize--;
 	//output
@@ -98,31 +98,33 @@ unsigned long long rotatingCalipers(Point* hull) {
 	if(finalhullsize <= 1) return 0;
 	if(finalhullsize == 2) return dist(hull[0], hull[1]);
 	int n = finalhullsize;
-	int k = 1;
 	printf("hull size %d\n", n);	
-	
-	//this may be naive? we need to find the two farthest points from eachother in the hull, and check rotation based on those.
-	
-	//finding the farthest vertex from hull[0] & hull[n-1]
-	while(  crossProduct(hull[n-1], hull[0], hull[(k+1) % n]) > crossProduct(hull[n-1], hull[0], hull[k])  ) {
-		k+=1;
-	}
+
+	//printf("k1: %d v1: %lld  ", (k+1)%n, crossProduct(hull[n-1], hull[0], hull[(k+1) % n]))
+	//printf("k2: %d v2: %lld \n", k, crossProduct(hull[n-1], hull[0], hull[k])) 
+
 
 	unsigned long long res = 0;
-	
+	int j = 1;
 	//check points from 0 to k
-	for(int i = 0; i<(k+1); i++) {
-		int j=(i+1) % n;
+	for(int i = 0; i<n; i++) {
+		int ni=(i+1) % n;
 		printf("Pair of:\n");
 		printf("Point %d: %llu, %llu \n", i, hull[i].x, hull[i].y);
-		printf("Point %d: %llu, %llu \n", (j+1)%n, hull[(j+1)%n].x, hull[(j+1)%n].y);
-		while(  crossProduct(hull[i], hull[(i+1) % n], hull[(j+1) % n]) > crossProduct(hull[i], hull[(i+1)%n], hull[j])  ) {
-			long long cp = dist(hull[i], hull[(j+1)%n]);
-			if(cp>res){
-				//printf("new largest at points:\n");
-				res = cp;
-			}
+		printf("Point %d: %llu, %llu \n", ni, hull[ni].x, hull[ni].y);
+		printf("Point %d: %llu, %llu \n", j, hull[j].x, hull[j].y);
+		while(  crossProduct(hull[i], hull[ni], hull[(j+1) % n]) > crossProduct(hull[i], hull[ni], hull[j])  ) {
 			j=((j+1)%n);
+		}
+		long long cp = dist(hull[i], hull[j]);
+		long long cp2 = dist(hull[ni], hull[j]);
+		printf("possible largest at points i, j: %lld\n", cp);
+		printf("possible largest at points ni, j: %lld\n", cp2);	
+		if(cp>res){
+			res = cp;
+		}
+		if(cp2>res){
+			res = cp2;
 		}
 	}
 	return res;
@@ -137,7 +139,11 @@ int main(int argc, char** argv) {
 		printf("Error opening file. Make sure file exists.\\n");
 		return -1; // Exit the program with an error
 	}
-	
+
+
+	//array for 2 actual points
+	int max[2];
+
 	//array for all points
 	Point* arr = 0;
 	int dataSize = 0;
@@ -151,13 +157,13 @@ int main(int argc, char** argv) {
 		arr = realloc(arr, dataSize*sizeof(Point));
 		(arr+dataSize-1)->x = strtol(strtok(buf, DELIM), NULL, 10);	
 		(arr+dataSize-1)->y = strtol(strtok(NULL, DELIM), NULL, 10);	
-				
+
 	}
 
 	Point* convexHull = convHull(arr, dataSize);
 	sum = rotatingCalipers(convexHull);
 
-	
+
 	printf("Full Hull Print:\n");
 	for(int i = 0; i<finalhullsize; i++) {
 		printf("Point %d: %llu, %llu \n", i, (convexHull+i)->x, (convexHull+i)->y);
